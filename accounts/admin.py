ea360213985a858af1f690ab2a0dbbb726b9aecf
Your Name <hosoekazuki@akane.waseda.jp>
2025-09-20 00:12:29 +0900

    管理画面にモデル追加

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from tweets.models import Tweet
+from tweets.models import Tweet, Like
 
-from .models import User
+from .models import User, Connection
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Tweet)
+admin.site.register(Like)
+admin.site.register(Connection)
